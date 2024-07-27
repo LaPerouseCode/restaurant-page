d54c33284ac33ddee741342c71d6b68a9a9b6e59
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2024-07-27 15:01:51 +1000

    added my github

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-import './styles.css';
-import loadHomepage from './pageLoad';
-import _ from 'lodash';
-import loadMenu from './menu';  // Import the menu module
-import loadContact from './contact';  // Import the contact module
+import "./style.css";
+import loadHomePage from "./load-home";
+import loadMenuPage from "./load-menu";
+import loadContactPage from "./load-contact";
 
-loadHomepage();
+const homeButton = document.querySelector('.home-button');
+const menuButton  = document.querySelector('.menu-button');
+const contactButton = document.querySelector('.contact-button');
 
+document.addEventListener("DOMContentLoaded", loadHomePage);
 
-// Get buttons and content div
-const homeButton = document.getElementById('home');
-const menuButton = document.getElementById('menu');
-const contactButton = document.getElementById('about');
-const contentDiv = document.getElementById('content');
-
-// Function to clear content and load a specific tab
-function loadTab(tabFunction) {
-  contentDiv.innerHTML = ''; // Clear current content
-  tabFunction(); // Load new tab content
-}
-
-// Event listeners for buttons
-homeButton.addEventListener('click', () => loadHomepage());
-menuButton.addEventListener('click', () => loadTab(loadMenu));
-contactButton.addEventListener('click', () => loadTab(loadContact));
-
-// function component() {
-//     const element = document.createElement('div');
-  
-//     // Lodash now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
-//   console.log("Index.js is working")
-//   alert('ALERT FROM INDEX.JS IS WORKING')
+homeButton.addEventListener("click", loadHomePage);
+menuButton.addEventListener("click", loadMenuPage);
+contactButton.addEventListener("click", loadContactPage);
